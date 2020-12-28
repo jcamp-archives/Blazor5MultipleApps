b83cf12a5aa1bf9f16114c546d2cd7010ce0f7c0
John Campion <john@brightshore.com>
2020-12-28 12:57:46 -0500

    Initial project

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # Blazor5MultipleApps
-Multiple Blazor apps in one server app
+
+The goal of this is to confirm whether I could host multiple WASM apps off of one server.
+
+### References
+[@Javiercn's repo with multiple blazor wasm apps](https://github.com/javiercn/BlazorMultipleApps/tree/13cefa97483641e08ee2c1d7eabee186147d4dd3)
+
+Updated with .NET 5.0 and added routing to weather forecast controller to allow both to use it.
---
+++ src/Blazor5MultipleApps/Server/Controllers/WeatherForecastController.cs
@@ -0,0 +1,41 @@
+﻿using Blazor5MultipleApps.Shared;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Blazor5MultipleApps.Server.Controllers
+{
+    [ApiController]
+    [Route("first/[controller]")]
+    [Route("second/[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            var rng = new Random();
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = rng.Next(-20, 55),
+                Summary = Summaries[rng.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+    }
+}
